240f5b521ec18ac5f1c0778c791026c8133fd36f
Etienne Batise <etienne.batise@gmail.com>
2014-05-27 11:47:33 +0200

    Ajout de la génération de couleur aléatoire

--- MiniGimp/src/fr/insarouen/asi/minigimp/Utilitaires.java
+++ MiniGimp/src/fr/insarouen/asi/minigimp/Utilitaires.java
@@ -1,6 +1,7 @@
 package fr.insarouen.asi.minigimp;
 
 import java.util.*;
+import java.awt.Color;
 
 public class Utilitaires {
     private static Random alea = new Random();
@@ -9,4 +10,12 @@ public class Utilitaires {
 	return 1+Math.abs( alea.nextInt() ) % max;
     }
 
+    public static Color randomColor() {
+        Random rand = new Random();
+        float r = rand.nextFloat();
+        float g = rand.nextFloat();
+        float b = rand.nextFloat();
+
+        return new Color(r, g, b).brighter();
+    }
 }

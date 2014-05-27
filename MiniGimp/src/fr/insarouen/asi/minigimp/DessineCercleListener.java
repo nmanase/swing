542f260f69a4a7d80843f17a6e44bbc8167eba5d
Etienne Batise <etienne.batise@gmail.com>
2014-05-27 11:51:09 +0200

    Intégration des dessins en drag'n'drop avec le dessin

--- MiniGimp/src/fr/insarouen/asi/minigimp/DessineCercleListener.java
+++ MiniGimp/src/fr/insarouen/asi/minigimp/DessineCercleListener.java
@@ -5,13 +5,33 @@ import java.awt.event.*;
 
 // TODO Listener qui modifie les données (dessinables) lors d'un click dans la zone de dessin
 public class DessineCercleListener extends MouseAdapter {
-  
-  
-  public DessineCercleListener(DessinablesData dessinables) {
-   
-  }
-     
-  
+
+
+    private DessinablesData dessinables;
+    private int x, y;
+
+    public DessineCercleListener(DessinablesData dessinables) {
+
+        this.dessinables = dessinables;
+
+    }
+
+    public void mousePressed(MouseEvent e) {
+        this.x = e.getX();
+        this.y = e.getY();
+    }
+
+    public void mouseReleased(MouseEvent e) {
+
+        int rX = e.getX();
+        int rY = e.getY();
+        int rayon =  (int)Math.sqrt(Math.pow(Math.abs(rX - this.x), 2) + Math.pow(Math.abs(rY-this.y), 2));
+
+        Cercle cerc =  new Cercle(Math.min(this.x, rX), Math.min(this.y, rY) ,rayon, Utilitaires.randomColor()); 
+        this.dessinables.addDessinable(cerc);
+
+    }
+
 }
 
 
--- MiniGimp/src/fr/insarouen/asi/minigimp/DessineRectangleListener.java
+++ MiniGimp/src/fr/insarouen/asi/minigimp/DessineRectangleListener.java
@@ -4,12 +4,40 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-// TODO Listener qui modifie les données (dessinables) lors d'un click dans la zone de dessin
+/**
+ * Listener qui modifie les données (dessinables) lors d'un click dans la zone de dessin
+ */
 public class DessineRectangleListener extends MouseAdapter {
-  
-  public DessineRectangleListener(DessinablesData dessinables) {
-  }
 
- 
+    private DessinablesData dessinables;
+    private int x, y;
+    
+    public DessineRectangleListener(DessinablesData dessinables) {
+
+        this.dessinables = dessinables;
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        this.x = e.getX();
+        this.y = e.getY();
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        int rX = e.getX();
+        int rY = e.getY();
+
+        // System.out.println(this.x + " , "+ this.y +" , "+ lastX +" , "+ lastY);        
+
+        //Rectangle rect = new Rectangle(Math.min(this.x, lastX), Math.min(this.y, lastY),
+         //           Math.max(this.x, lastX), Math.max(this.y, lastY), Color.RED);
+        Rectangle rect = new Rectangle(Math.min(this.x, rX), Math.min(this.y, rY),
+                Math.abs(rX-this.x), Math.abs(rY-this.y), Utilitaires.randomColor());
+
+        this.dessinables.addDessinable(rect);
+                    
+    }
 }
 
